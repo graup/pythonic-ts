eb8b33b9d693447e0dd37f1da90bab20ef1d99a0
Paul Grau <paul@graycoding.com>
2021-04-08 18:08:30 +0900

    Configure babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
-import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import depsExternal from 'rollup-plugin-node-externals';
+
+const extensions = ['.ts'];
+
+const plugins = [
+  depsExternal({ deps: true, include: [/@babel\/runtime/] }),
+  nodeResolve({ extensions }),
+  babel({
+    extensions,
+    babelHelpers: 'runtime',
+    exclude: ['**/*.test.ts'],
+  }),
+];
 
 export default [
   {
     input: 'src/index.ts',
-    plugins: [
-      typescript({
-        declaration: true,
-        declarationDir: 'dist',
-        exclude: ['**/*.test.ts'],
-      }),
-    ],
+    plugins,
     preserveModules: true,
     output: [
       {
